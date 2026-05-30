57cd827d280747f4f087eb038853cc8882c61038
akhi9273 <r0764168@gmail.com>
2026-05-30 10:11:14 +0530

    Remove file from git

--- Experiment1.js
+++ Experiment1.js
@@ -3,7 +3,6 @@ window.onload = function () {
     console.log("Welcome to My Website");
 };
 document.querySelectorAll(".navbar a").forEach(link => {
-    link.addEventListener("click", function () {
         alert("You clicked: " + this.textContent);
     });
 });
